7e35324421b8afa81425ba8815038a80326b3b85
RenataEmy <renata.emy2012@gmail.com>
2026-03-29 16:00:00 -0300

    feat: register product finally

--- client/web/src/components/Forms.tsx
+++ client/web/src/components/Forms.tsx
@@ -32,72 +32,78 @@ export function Forms(){
 
 
     return(
-        <Card className={"max-w-3xl w-full mx-auto"}>
-            <form className={"flex flex-col justify-center gap-9"}
-            onSubmit={handleSubmit(createProduct)}>
-                <div className="flex flex-col gap-3">
-                    <h2 className="text-xl text-[var(--color-text-primary)]">Informações Básicas</h2>
-                    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                        <InputField id="name" type="text"  placeholder="Nome do produto" 
-                        className={errors?.name && "input-error"}
-                        {...register("name", {required: true})}>
-                            Nome do Produto *
-                        </InputField>
-                        {errors?.name?.type === "required" &&(
-                            <span><p className="error-message">Campo obrigatório</p></span>
-                        )}
-                        <InputField id="sku" type="text" placeholder="SKU"
-                        className={errors?.sku && "input-error"} 
-                        {...register("sku", {required: true})}>
-                            SKU *
-                        </InputField>
-                        {errors?.sku?.type === "required" &&(
-                            <span><p className="error-message">Campo obrigatório</p></span>
-                        )}
-                    </div>
-                </div>
-    
-                <div className="flex flex-col gap-3">
-                    <h2 className="text-xl text-[var(--color-text-primary)]">Estoque e Preço</h2>
-                    <div className="grid grid-cols-2 gap-4">
-                        <InputField id="quantity" type="number" step="1" min="0"  placeholder="Quantidade" 
-                        className={errors?.quantity && "input-error"}
-                        {...register("stock", {required: true, valueAsNumber: true})}>
-                            Quantidade *
-                        </InputField>
-                        {errors?.quantity?.type === "required" &&(
-                            <span><p className="error-message">Campo obrigatório</p></span>
-                        )}
-                        <InputField id="price" type="number" step="0.01" placeholder="Preço"
-                        className={errors?.price && "input-error"}
-                        {...register("price", {required: true, valueAsNumber: true})}>
-                            Preço (R$) *
-                        </InputField>
-                        {errors?.price?.type === "required" &&(
-                            <span><p className="error-message">Campo obrigatório</p></span>
-                        )}
-                    </div>
-                </div>
-                <div className="flex flex-col gap-3">
-                    <h2 className="text-xl text-[var(--color-text-primary)]">Descrição</h2>
-                    <label htmlFor="description"></label>
-                    <textarea id="description" placeholder="Descrição" {...register("description")}
-                    className="w-full border border-[var(--color-border)] rounded-md px-3 py-2 focus:outline-none focus:border-purple-600 focus:ring-1 focus:ring-purple-600">
-                    </textarea>
-                </div>
+        <div className='flex flex-col gap-5'>
+            <div className='h-[1px] bg-[var(--color-border)]'></div>
 
-                <div className="flex flex-row gap-3">
-                    <label htmlFor="is_active">O produto está ativo?</label>
-                    <input id="is_active" type="checkbox" 
-                    {...register("is_active")}>
-                    </input>
-                </div>
+            <Card className={"max-w-3xl w-full mx-auto"}>
+                <form className={"flex flex-col justify-center gap-9"}
+                onSubmit={handleSubmit(createProduct)}>
 
-                <div className="flex flex-row justify-end gap-4 mt-2">
-                    <Button type="button" variant="secondary" text="Cancelar" onClick={handleDashboard}/>
-                    <Button text="Cadastrar Produto" icon={Save} type="submit"/>
-                </div>
-            </form>
-        </Card>
+                    <div className="flex flex-col gap-3">
+                        <h2 className="text-xl text-[var(--color-text-primary)]">Informações Básicas</h2>
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+
+                            <InputField id="name" type="text"  placeholder="Nome do produto" 
+                            className={errors?.name && "input-error"}
+                            error={errors?.name}
+                            {...register("name", {required: "Campo obrigatório"})}>
+                                Nome do Produto *
+                            </InputField>
+
+                            <InputField id="sku" type="text" placeholder="SKU"
+                            className={errors?.sku && "input-error"}
+                            error={errors?.sku}
+                            {...register("sku", {required: "Campo obrigatório"})}>
+                                SKU *
+                            </InputField>
+
+                        </div>
+                    </div>
+        
+                    <div className="flex flex-col gap-3">
+                        <h2 className="text-xl text-[var(--color-text-primary)]">Estoque e Preço</h2>
+                        <div className="grid grid-cols-2 gap-4">
+
+                            <InputField id="stock" type="number" step="1" min="0"  placeholder="Quantidade" 
+                            className={errors?.stock && "input-error"}
+                            error={errors?.stock}
+                            {...register("stock", {required: "Campo obrigatório", valueAsNumber: true})}>
+                                Quantidade *
+                            </InputField>
+
+
+                            <InputField id="price" type="number" step="0.01" placeholder="Preço"
+                            className={errors?.price && "input-error"}
+                            error={errors?.price}
+                            {...register("price", {required: "Campo obrigatório", valueAsNumber: true})}>
+                                Preço (R$) *
+                            </InputField>
+
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-3">
+                        <h2 className="text-xl text-[var(--color-text-primary)]">Descrição</h2>
+                        <label htmlFor="description"></label>
+                        <textarea id="description" placeholder="Descrição" {...register("description")}
+                        className="w-full border border-[var(--color-border)] rounded-md px-3 py-2 focus:outline-none focus:border-purple-600 focus:ring-1 focus:ring-purple-600">
+                        </textarea>
+                    </div>
+
+                    <div className="flex flex-row gap-3">
+                        <label htmlFor="is_active">O produto está ativo?</label>
+                        <input id="is_active" type="checkbox" 
+                        {...register("is_active")}>
+                        </input>
+                    </div>
+
+                    <div className="flex flex-row justify-end gap-4 mt-2">
+                        <Button type="button" variant="secondary" text="Cancelar" onClick={handleDashboard}/>
+                        <Button text="Cadastrar Produto" icon={Save} type="submit"/>
+                    </div>
+                </form>
+            </Card>
+        </div>
+
     )
 }
--- client/web/src/components/InputField.tsx
+++ client/web/src/components/InputField.tsx
@@ -1,16 +1,20 @@
 import { forwardRef } from "react";
 
-export const InputField = forwardRef(({children, id, type, className, ...props}, ref) =>{
+export const InputField = forwardRef(({children, id, type, error, className, ...props}, ref) =>{
     return(
         <div className="flex flex-col gap-3">
             <label className={`text-sm text-[var(--color-text-primary)] ${className ? className : ''}`} htmlFor={id}>
                 { children }
             </label>
-            <input type={type} id={id} ref={ref} {...props}
-            className="w-full border border-[var(--color-border)] rounded-md px-3 py-2 focus:outline-none focus:border-[var(--color-border-focus)] focus:ring-1 focus:ring-[var(--color-border-focus)]"/>
+            <input type={type} id={id} ref={ref} error={error} {...props}
+            className={`w-full border rounded-md px-3 py-2 focus:outline-none focus:ring-1 
+            ${error ? 'border-red-500 focus:border-red-500 focus:ring-red-500' : 'focus:border-[var(--color-border-focus)] focus:ring-[var(--color-border-focus)]'}`}/>
+
+            {error && (
+                <span className="text-red-500 text-sm mt-1">{error.message}</span>
+            )}
         </div>
     );
 }
 );
 
-InputField.displayName = "InputField";
