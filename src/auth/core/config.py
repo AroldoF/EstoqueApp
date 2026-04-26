ac1a3e8f3a44af496d2ca3a35ddf78a0c61c8650
RenataEmy <renata.emy2012@gmail.com>
2026-04-26 12:34:20 -0300

    feat: include functions token

--- src/auth/core/config.py
+++ src/auth/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     SECRET_KEY: str 
     ALGORITHM: str 
+    ACCESS_TOKEN_EXPIRE_MINUTES = int(str)
 
     DATABASE_URL: str = "sqlite+aiosqlite:///./orders_dev.db"
     
