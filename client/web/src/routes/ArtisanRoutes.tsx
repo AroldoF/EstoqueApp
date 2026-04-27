adb8c18ca5cfc6fe6006bf4d8050348b70f47bc5
MariaAless <alessandraaquino2003@gmail.com>
2026-04-27 17:39:59 -0300

    feat(routes): implement PrivateRoute wrapper for protected access

--- client/web/src/routes/ArtisanRoutes.tsx
+++ client/web/src/routes/ArtisanRoutes.tsx
@@ -8,20 +8,32 @@ import ProductUpdate from "../pages/ProductUpdate"
 import { Login } from "../pages/Login"
 import { Register } from "../pages/Register"
 
+
+import { Navigate, Outlet } from "react-router";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
+function PrivateRoute() {
+  const { isAuthenticated } = useContext(AuthContext);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+}
+
 export function ArtisanRoutes(){
 
   return(
- <Routes>
-   
-    <Route path="/login" element={<Login />} />
-    <Route path="/signup" element={<Register />} />
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
 
-   
-    <Route path="/" element={<AppLayout/>}>
-      <Route path="/" element={<Dashboard/>}/>
-      <Route path="/register" element={<ProductRegistration/>}/>
-      <Route path="/update/:productId/" element={<ProductUpdate/>}/>
-    </Route>
-  </Routes>
+     
+      <Route element={<PrivateRoute/>}>
+        <Route path="/" element={<AppLayout/>}>
+          <Route path="/" element={<Dashboard/>} />
+          <Route path="/register" element={<ProductRegistration/>} />
+          <Route path="/update/:productId/" element={<ProductUpdate/>} />
+        </Route>
+      </Route>
+    </Routes>
   )
 }
