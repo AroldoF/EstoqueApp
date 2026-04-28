f7ca681af734232fa2df50429ff69cb5a4fcae34
MariaAless <alessandraaquino2003@gmail.com>
2026-04-28 09:55:42 -0300

    feat: implement product CRUD proxy and auth gRPC integration

--- api-gateway/main.py
+++ api-gateway/main.py
@@ -4,6 +4,7 @@ import auth_pb2_grpc
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import httpx
 
 app = FastAPI()
 
@@ -65,6 +66,67 @@ async def signup(data: RegisterSchema):
     except grpc.RpcError:
         raise HTTPException(status_code=500, detail="Erro ao falar com serviço de Auth")
 
+
+
+PRODUCT_SERVICE_URL = "http://localhost:8001/api"
+
+@app.get("/api/product")
+async def list_products():
+    async with httpx.AsyncClient() as client:
+        try:
+            
+            response = await client.get(f"{PRODUCT_SERVICE_URL}/product")
+            return response.json()
+        except Exception:
+            raise HTTPException(status_code=503, detail="Erro ao buscar produtos no estoque")
+        
+
+@app.post("/api/product") 
+async def create_product(product_data: dict):
+    async with httpx.AsyncClient() as client:
+        try:
+           
+            response = await client.post(f"{PRODUCT_SERVICE_URL}/product", json=product_data)
+            return response.json()
+        except Exception:
+            raise HTTPException(status_code=503, detail="Erro ao salvar no estoque")
+
+
+@app.get("/api/product/{product_id}")
+async def get_product(product_id: int):
+    async with httpx.AsyncClient() as client:
+        try:
+            
+            response = await client.get(f"{PRODUCT_SERVICE_URL}/product/{product_id}/")
+            if response.status_code == 404:
+                raise HTTPException(status_code=404, detail="Produto não encontrado no banco")
+            return response.json()
+        except Exception:
+            raise HTTPException(status_code=503, detail="Erro ao buscar produto")
+
+
+@app.patch("/api/product/{product_id}")
+async def update_product(product_id: int, product_data: dict):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.patch(
+                f"{PRODUCT_SERVICE_URL}/product/{product_id}/", 
+                json=product_data
+            )
+            return response.json()
+        except Exception:
+            raise HTTPException(status_code=503, detail="Erro ao atualizar produto")
+
+
+@app.delete("/api/product/{product_id}")
+async def delete_product(product_id: int):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.delete(f"{PRODUCT_SERVICE_URL}/product/{product_id}/")
+            return {"message": "Produto deletado com sucesso"}
+        except Exception:
+            raise HTTPException(status_code=503, detail="Erro ao deletar produto")
+        
 @app.get("/")
 def read_root():
     return {"message": "API Gateway Online"}
