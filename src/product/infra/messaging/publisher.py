358058150962d140d25ebc9f742bcd5fb4122124
MariaAless <alessandraaquino2003@gmail.com>
2026-06-16 23:37:10 -0300

    fix(messaging): initialize broker connection for event publishing

--- src/product/infra/messaging/publisher.py
+++ src/product/infra/messaging/publisher.py
@@ -1,26 +1,27 @@
-from faststream.rabbit import RabbitExchange
-
+from faststream.rabbit import RabbitBroker
 from .broker import broker
 from .constants import Exchange, RoutingKey
 from .events import LowStockEvent
+from .exchange import exchange
 
-exchange = RabbitExchange(Exchange.STOCK)
 
 async def publish_low_stock(event: LowStockEvent):
 
-    
     print("evento")
     print(event.model_dump())
 
-    
-    async with broker:
-       
-       await broker.publish(
+    await broker.connect()
+    await broker.start()
+    await broker.declare_exchange(exchange=exchange)
+
+    await broker.publish(
             message=event.model_dump(),
             exchange=exchange,
             routing_key=RoutingKey.STOCK_KEY,
         )
 
+    return event.model_dump()
+
   
 
-    # print("publicado")
+   
